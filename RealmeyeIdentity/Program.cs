974aa16fb4e3a55cc20240d6e2fabadbcc77caaf
Haleliasa <gosh950@gmail.com>
2023-11-17 20:48:03 +0100

    change default port; remove comments

--- RealmeyeIdentity/Program.cs
+++ RealmeyeIdentity/Program.cs
@@ -2,7 +2,6 @@ using RealmeyeIdentity.Authentication;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRouting(options =>
 {
     options.LowercaseUrls = true;
@@ -25,16 +24,6 @@ builder.Services.Configure<CodeGeneratorOptions>(config.GetSection("CodeGenerato
 UserBsonMap.Register();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-//if (!app.Environment.IsDevelopment())
-//{
-//    //app.UseExceptionHandler("/Home/Error");
-//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-//    app.UseHsts();
-//}
-
-//app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 
